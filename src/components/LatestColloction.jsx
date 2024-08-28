92090a420073487b9d9b59312e66eb0a4902649a
sayan959 <shayonbiswas111@gmail.com>
2024-08-28 12:18:16 +0600

    first commit

---
+++ src/components/LatestColloction.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContex'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestColloction = () => {
+    const { products } = useContext(ShopContext);
+    const [latestProducts,setLatestProducts] = useState([]);
+
+    useEffect(()=>{
+        setLatestProducts(products.slice(0,10));
+    },[])
+    
+    
+    
+  return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTION'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sed vel recusandae aperiam nulla esse!
+            </p>
+            
+        </div>
+
+        {/* Rending Products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                latestProducts.map((item,index)=>(
+                    <ProductItem  key={index} id={item._id} image={item.image} name={item.name} price={item.price}  />
+                ))
+            }
+        </div>
+
+    </div>
+  )
+}
+
+export default LatestColloction
